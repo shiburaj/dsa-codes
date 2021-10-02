b1c1b8a01a6eac81e67ca64afcc44a1c8f9d38bb
Shiburaj Pappu <powerupneo@gmail.com>
2021-10-02 19:47:34 +0530

    Linked List Implementation

--- Module 3 - Linked List/sll_adt.c
+++ Module 3 - Linked List/sll_adt.c
@@ -1,9 +1,71 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node * next;
+};
+
+struct Node * head = NULL;
+
+void add(){
+    int value;
+    printf("Enter a Value:");
+    scanf("%d", &value);
+    struct Node * temp;
+
+    struct Node *nt = malloc(sizeof(struct Node));
+    nt->data = value;
+    nt->next = NULL;
+
+    if(head == NULL){
+        head = nt;
+    }else{
+        temp = head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = nt;
+    }
+
+    printf("Node Added.\n");
+}
+
+void display(){
+    printf("LL :");
+    struct Node * temp;
+    temp = head;
+    do{
+        printf("%d-->", temp->data);
+        temp = temp->next;
+    }while(temp->next != NULL);
+    printf("%d-->NULL", temp->data);
+
+}
+
+int main()
+{
+    int choice;
+    printf("Enter an Choice:\n1.Add , 2.Display, 3. Exit\n\n");
+    while(1){
+        printf("Enter Choice:");
+        scanf("%d", &choice);
+
+        switch(choice){
+            case 1:
+                add();
+                break;
+            case 2:
+                display();
+                break;
+            case 3:
+                return 0;
+            default:
+                printf("Wrong Option.");
+        }
+
+    }
 
 
-int main(){
-	printf("Hi");
-
-
-	return 0;
+    return 0;
 }
--- README.md
+++ README.md
@@ -9,3 +9,6 @@ No Programs
 ## *Module 2 : Stack and Queue*
 1. [Stack ADT](/Module%202%20-%20Stack%20&%20Queue/stack_adt.c)
 2. [Queue ADT](/Module%202%20-%20Stack%20&%20Queue/queue_adt.c)
+
+## *Module 3 : Linked List*
+1. [Linked List ADT](/Module%202%20-%20Linked%20List/sll_adt.c)
